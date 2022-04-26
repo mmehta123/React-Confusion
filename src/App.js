6159aa39a6e0cb98fdf3e02f98d311736cb8abde
mmehta123 <mohitmehta285@gmail.com>
2022-04-26 11:26:43 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
+import Menu from "./components/MenuComponent";
+import {DISHES} from "./shared/dishes"
+import { Component } from "react";
+
+class App extends Component {
+  render(){
+
+    return (
+      <div className="App">
+      <Navbar dark color="primary ">
+        <div className="container">
+          <NavbarBrand href="#">Restorent Confusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
+  );
+}
+}
+
+export default App;
